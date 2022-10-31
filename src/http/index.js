a7b30b6e46fcddad33c1fd9bf5ef95ca6a52603b
havriluke <havrylyuk.post@gmail.com>
2022-10-31 23:16:57 +0200

    cors12.1

--- src/http/index.js
+++ src/http/index.js
@@ -10,7 +10,7 @@ const $authHost = axios.create({
 
 const authInterceptor = config => {
     config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
-    config.headers['Access-Control-Allow-Origin'] = '*'
+    config.headers['Access-Control-Allow-Origin'] = 'https://social-kreep.herokuapp.com'
     config.headers['Access-Control-Allow-Methods'] = "OPTIONS,PATCH,GET,PUT,POST,DELETE"
     config.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept"
     return config
