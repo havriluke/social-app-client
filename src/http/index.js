55d54646cb0184cf44f61586de4a6215c8b9ab17
havriluke <havrylyuk.post@gmail.com>
2022-10-31 23:06:51 +0200

    cors10

--- src/http/index.js
+++ src/http/index.js
@@ -10,6 +10,9 @@ const $authHost = axios.create({
 
 const authInterceptor = config => {
     config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+    config.headers['Access-Control-Allow-Origin'] = '*'
+    config.headers['Access-Control-Allow-Methods'] = "OPTIONS,PATCH,GET,PUT,POST,DELETE"
+    config.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept"
     return config
 }
 
